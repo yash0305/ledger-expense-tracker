1154cdab4674c1fd5c740703d4131c920cd81aa4
Yash0305 <kesharwaniyash20@gmail.com>
2026-07-30 12:43:35 +0530

    Update API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+
+const API_URL = "https://ledger-expense-tracker-p59q.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
